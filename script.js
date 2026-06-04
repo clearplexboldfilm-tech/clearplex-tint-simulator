967fd7f9ee0e2ecc5d07a2682cc9679b902531a0
clearplexboldfilm-tech <clearplexboldfilm@gmail.com>
2026-06-04 17:51:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function changeTint(value){
+
     document.getElementById("tintValue").innerText = value + "%";
+
+    let filename;
+
+    if(value === "5"){
+        filename = "suv_05.jpg";
+    }else{
+        filename = "suv_" + value + ".jpg";
+    }
+
+    document.getElementById("carImage").src = filename;
 }
