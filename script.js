be5a56965763d71124729542567661675c4ae12f
clearplexboldfilm-tech <clearplexboldfilm@gmail.com>
2026-06-05 12:04:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,8 +32,32 @@ function updateInfo() {
         currentTint === "20" ||
         currentTint === "35"
     ) {
-        badge.innerText = "🔥 Most Popular";
+        badge.innerText = "⭐ MOST POPULAR";
     } else {
         badge.innerText = "";
     }
+
+    document
+        .querySelectorAll(".vehicle-selector button")
+        .forEach(btn => btn.classList.remove("active"));
+
+    if(currentVehicle === "suv"){
+        document
+            .getElementById("suvBtn")
+            .classList.add("active");
+    } else {
+        document
+            .getElementById("sedanBtn")
+            .classList.add("active");
+    }
+
+    document
+        .querySelectorAll(".buttons button")
+        .forEach(btn => btn.classList.remove("active"));
+
+    document
+        .getElementById("tint" + currentTint)
+        .classList.add("active");
 }
+
+updateImage();
